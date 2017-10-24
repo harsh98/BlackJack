f193863bf8aa6fbf12b85d78df054311aa9668b5
lilave232 <averypozzobon@gmail.com>
2017-10-23 21:59:32 -0400

    updated classes

--- eclipse-workspace/BlackJack1.0/src/game/Main.java
+++ eclipse-workspace/BlackJack1.0/src/game/Main.java
@@ -6,7 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		deck.StartGame();
-		System.out.println(deck.ShuffleDeck());
 		System.out.println(user.GetHand());
 	}
 
