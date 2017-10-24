6e918e4c09cdacd01791699d7a0367eda026bf3f
Harsh <Harsh@10.81.119.165>
2017-10-23 20:29:27 -0400

    commit 4

--- eclipse-workspace/BlackJack1.0/src/test.java
+++ eclipse-workspace/BlackJack1.0/src/test.java
@@ -3,7 +3,8 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		int temp = 5;
+		System.out.println(4 * temp);
 	}
 
 }
