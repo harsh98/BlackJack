d9893af737c4f8f8f64a607dfe4bc72573dbeef3
lilave232 <averypozzobon@gmail.com>
2017-12-22 10:24:12 -0500

    Blackjack

--- eclipse-workspace/BlackJack1.0/src/game/Main.java
+++ eclipse-workspace/BlackJack1.0/src/game/Main.java
@@ -1,11 +1,19 @@
 package game;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 import game.Deck;
+
 public class Main {
-	static Deck deck = new Deck();
 	static User user = new User();
+	static Dealer dealer = new Dealer();
+	static Deck deck = new Deck();
+	static int win = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		deck.StartGame();
+		dealer.PrintInitialHand();
+		user.PrintInitialHand();
+		user.UserTurn();
 	}
-
 }
